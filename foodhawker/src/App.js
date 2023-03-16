bb1787bc102b6487a57e89d7568da7bb260209b3
Minh Nguyen <gin411@Minhs-MacBook-Pro-2.local>
2023-03-16 15:55:20 -0400

    Fix styling for the main page

--- foodhawker/src/App.js
+++ foodhawker/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import StorePicker from './components/StorePicker';
-import FoodMenu from './components/FoodMenu';
-import Inventory from './components/Inventory';
-import Order from './components/Order';
+import logo from "./logo.svg";
+import "./App.css";
+import StorePicker from "./components/StorePicker";
+import FoodMenu from "./components/FoodMenu";
+import Inventory from "./components/Inventory";
+import Order from "./components/Order";
 function App() {
   return (
     <div
       className=" flex flex-shrink-1 p-8
      relative bg-white shadow-md min-h-screen"
     >
-      {/* <StorePicker/> */}
-      <div className="w-1/3 p-4 h-full border-8 border-double border-gray-900">
-        <FoodMenu />
+      {/* <StorePicker /> */}
+      <div className=" p-4 border-8 border-double border-gray-900 flex-1">
+        <FoodMenu className=" h-full" />
       </div>
-      <div className="w-1/3 p-4 h-full border-8 border-double border-gray-900">
-        <Inventory />
+      <div className=" p-4 border-8 border-double border-gray-900 flex-1">
+        <Inventory className=" h-full" />
       </div>
-      <div className="w-1/3 p-4 h-full border-8 border-double border-gray-900">
-        <Order />
+      <div className=" p-4  border-8 border-double border-gray-900 flex-1">
+        <Order className=" h-full" />
       </div>
     </div>
   );
--- foodhawker/src/components/Header.js
+++ foodhawker/src/components/Header.js
@@ -3,14 +3,16 @@ import React from "react";
 function Header() {
   return (
     <div className="">
-      <header>
+      <header className="top">
         <h1>
           Today
           <span className="text-9xl leading-7">'s Foods</span>
         </h1>
       </header>
-      <h3 className="tagline text-yellow-500">
-        <span>Just As Good As Your Homemade's</span>
+      <h3 className="text-yellow-500 relativw z-2">
+        <span className="">
+          Just As Good As Your Homemade's
+        </span>
       </h3>
     </div>
   );
