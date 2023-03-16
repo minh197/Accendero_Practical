d93aa64bbc35e0fad9b6e4e77c8256d84496821e
Minh Nguyen <gin411@Minhs-MacBook-Pro-2.local>
2023-03-16 18:01:48 -0400

    use react router to navigate from the welcome to main page

--- foodhawker/src/components/StorePicker.js
+++ foodhawker/src/components/StorePicker.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
 function StorePicker() {
+  const navigate = useNavigate();
+  const myInput = React.createRef();
+  const [goToHawker, setGoToHawker] = useState(() => {
+    function handleGoToHawker(event) {
+      //stop the form from auto submit
+      event.preventDefault();
+      // get the text from the user's input
+      const userInput = myInput.current.value;
+      //change the page to /hawker/whatever-they-entered
+      navigate(`hawker/${userInput}`);
+    }
+    return handleGoToHawker;
+  });
 
-//     // myInput = React.createRef();
-
-// //   function goToHawker(event) {
-// //     //1. stop the form from submitting
-// //     event.preventDefault();
-// //     //2. get the text from the input
-
-//   }
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen bg-neutral-400	">
       <div className=" max-w-500 mx-auto p-8 border-2 border-black flex justify-between">
-        <form className="store-selector">
-          <h2 className="text-red-400	">
-            Please Enter The Name of Your Food Hawker
-          </h2>
+        <form className="store-selector" onSubmit={goToHawker}>
+          <h2>Please Enter The Name of Your Food Hawker</h2>
           <input
-        //   ref={this.myInput}
+            ref={myInput}
             className="w-full border-2 bg-slate-200	 text-center"
             type="text"
             required
